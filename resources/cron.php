907984cafd2793fb3ff9282ec094c22b172a16a7
Alex White <alex@picotory.com>
2017-07-19 14:55:43 +0100

    Improved alert sending

--- resources/cron.php
+++ resources/cron.php
@@ -3,116 +3,124 @@
 	//ini_set('display_startup_errors', 1);
 	//error_reporting(E_ALL);
 
-	include 'cfg/config.inc.php';
-	
-	# Load the config file
-	$mCfg = $Montr->LoadConfig();
-	
-	# Server load
-	$MemoryUsage = $Montr->getMemory();
-	
-	# Server load
-	$ServerLoad = $Montr->getLoad();
-	
-	# Disk limits are done separate due to multiple disks
-	foreach($mCfg['disks'] as $Disk) {
-		# Initialise as disk_usage
-		$type = 'disk_usage';
-		
-		# Disk usage
-		$DiskUsage = $Montr->getDisk($Disk['location']);
-		
-		# Over limits?
-		if(!empty($Disk['limit']) && $Disk['limit'] > 0) {
-			if($DiskUsage['percent'] > $Disk['limit']) {
-				# Check if the alert was sent before
-				if(!file_exists(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']))) {
-					fopen(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']), 'w');
-					
-					# Alert was not sent before, send it
-					$Montr->sendAlerts($type . ' - ' . $Montr->makeSlug($Disk['name']), $mCfg['contacts'], $mCfg);
-					
-					# Write to the daily log file
-					$Montr->WriteLog($type . ' - ' . $Montr->makeSlug($Disk['name']), $Disk['limit']);
-				}
-			}else{
-				# Coming out of an alarm? Remove the alarm file
-				if(file_exists(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']))) {
-					unlink(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']));
-					
-					# Send an alert once it's cleared?
-					if($Cfg_general['alert_on_clear']) {
+	# Only allow to be run via cli
+	if(php_sapi_name() == "cli") {
+		include 'cfg/config.inc.php';
+
+		# Load the config file
+		$mCfg = $Montr->LoadConfig();
+
+		# Server load
+		$MemoryUsage = $Montr->getMemory();
+
+		# Server load
+		$ServerLoad = $Montr->getLoad();
+
+		# Disk limits are done separate due to multiple disks
+		foreach($mCfg['disks'] as $Disk) {
+			# Initialise as disk_usage
+			$type = 'disk_usage';
+
+			# Disk usage
+			$DiskUsage = $Montr->getDisk($Disk['location']);
+
+			# Over limits?
+			if(!empty($Disk['limit']) && $Disk['limit'] > 0) {
+				if($DiskUsage['percent'] > $Disk['limit']) {
+					# Check if the alert was sent before
+					if(!file_exists(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']))) {
+						fopen(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']), 'w');
+
 						# Alert was not sent before, send it
-						$Montr->sendAlerts($type . '_cleared - ', $mCfg['contacts'], $mCfg);
+						$Montr->sendAlerts($type . ' - ' . $Montr->makeSlug($Disk['name']), $mCfg['contacts'], $mCfg);
+
+						# Write to the daily log file
+						$Montr->WriteLog($type . ' - ' . $Montr->makeSlug($Disk['name']), $Disk['limit']);
 					}
-					
-					# Write to the daily log file
-					$Montr->WriteLog($type . '_cleared');
+				}else{
+					# Coming out of an alarm? Remove the alarm file
+					if(file_exists(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']))) {
+						unlink(__DIR__ . '/data/disk_usage_' . $Montr->makeSlug($Disk['name']));
+
+						# Send an alert once it's cleared?
+						if($Cfg_general['alert_on_clear']) {
+							# Alert was not sent before, send it
+							$Montr->sendAlerts($type . '_cleared - ', $mCfg['contacts'], $mCfg);
+						}
+
+						# Write to the daily log file
+						$Montr->WriteLog($type . '_cleared');
+					}
 				}
 			}
 		}
-	}
-	
-	# Loop through all our limits
-	foreach($mCfg['limits'] as $type => $limit) {
-		# Memory usage
-		if($type == 'memory_usage' && $limit > 0) {
-			if($MemoryUsage['percent'] > $limit) {
-				# Check if the alert was sent before
-				if(!file_exists(__DIR__ . '/data/memory_usage')) {
-					fopen(__DIR__ . '/data/memory_usage', 'w');
-					
-					# Alert was not sent before, send it
-					$Montr->sendAlerts($type, $mCfg['contacts'], $mCfg);
-					
-					# Write to the daily log file
-					$Montr->WriteLog($type, $mCfg['limits']);
-				}
-			}else{
-				# Coming out of alarm? Remove the alarm file 
-				if(file_exists(__DIR__ . '/data/memory_usage')) {
-					unlink(__DIR__ . '/data/memory_usage');
-					
-					# Send an alert once it's cleared?
-					if($Cfg_general['alert_on_clear']) {
+
+		# Loop through all our limits
+		foreach($mCfg['limits'] as $type => $limit) {
+			# Memory usage
+			if($type == 'memory_usage' && $limit > 0) {
+				if($MemoryUsage['percent'] > $limit) {
+					# Check if the alert was sent before
+					if(!file_exists(__DIR__ . '/data/memory_usage')) {
+						fopen(__DIR__ . '/data/memory_usage', 'w');
+
 						# Alert was not sent before, send it
-						$Montr->sendAlerts($type . '_cleared', $mCfg['contacts'], $mCfg);
+						$Montr->sendAlerts($type, $mCfg['contacts'], $mCfg);
+
+						# Write to the daily log file
+						$Montr->WriteLog($type, $mCfg['limits']);
 					}
-					
-					# Write to the daily log file
-					$Montr->WriteLog($type . '_cleared');
+				}else{
+					# Coming out of alarm? Remove the alarm file
+					if(file_exists(__DIR__ . '/data/memory_usage')) {
+						unlink(__DIR__ . '/data/memory_usage');
+
+						# Send an alert once it's cleared?
+						if($Cfg_general['alert_on_clear']) {
+							# Alert was not sent before, send it
+							$Montr->sendAlerts($type . '_cleared', $mCfg['contacts'], $mCfg);
+						}
+
+						# Write to the daily log file
+						$Montr->WriteLog($type . '_cleared');
+					}
+				}
+			}
+
+			# Load alerts
+			if($type == 'load_alert' && $limit > 0) {
+				if($ServerLoad['one'] > $limit) {
+					# Check if the alert was sent before
+					if(!file_exists(__DIR__ . '/data/load_alert')) {
+						fopen(__DIR__ . '/data/load_alert', 'w');
+
+						# Alert was not sent before, send it
+						$Montr->sendAlerts($type, $mCfg['contacts'], $mCfg);
+
+						# Write to the daily log file
+						$Montr->WriteLog($type, $mCfg['limits']);
+					}
+				}else{
+					# Coming out of alarm? Remove the alarm file
+					if(file_exists(__DIR__ . '/data/load_alert')) {
+						unlink(__DIR__ . '/data/load_alert');
+
+						# Send an alert once it's cleared?
+						if($Cfg_general['alert_on_clear']) {
+							# Alert was not sent before, send it
+							$Montr->sendAlerts($type . '_cleared', $mCfg['contacts'], $mCfg);
+						}
+
+						# Write to the daily log file
+						$Montr->WriteLog($type . '_cleared');
+					}
 				}
 			}
 		}
-		
-		# Load alerts
-		if($type == 'load_alert' && $limit > 0) {
-			if($ServerLoad['one'] > $limit) {
-				# Check if the alert was sent before
-				if(!file_exists(__DIR__ . '/data/load_alert')) {
-					fopen(__DIR__ . '/data/load_alert', 'w');
-					
-					# Alert was not sent before, send it
-					$Montr->sendAlerts($type, $mCfg['contacts'], $mCfg);
-					
-					# Write to the daily log file
-					$Montr->WriteLog($type, $mCfg['limits']);
-				}
-			}else{
-				# Coming out of alarm? Remove the alarm file
-				if(file_exists(__DIR__ . '/data/load_alert')) {
-					unlink(__DIR__ . '/data/load_alert');
-					
-					# Send an alert once it's cleared?
-					if($Cfg_general['alert_on_clear']) {
-						# Alert was not sent before, send it
-						$Montr->sendAlerts($type . '_cleared', $mCfg['contacts'], $mCfg);
-					}
-					
-					# Write to the daily log file
-					$Montr->WriteLog($type . '_cleared');
-				}
-			}
-		}
-	}
+        
+        # Write when the cron last ran
+        $Montr->WriteCronTime(time());
+	}else{
+        echo 'Can not be run directly.';
+    }
 ?>
