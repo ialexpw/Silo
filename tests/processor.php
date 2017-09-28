3d17b4d36a550bf077ae6ad838306dd4c9eca8a5
Alex White <alex@picotory.com>
2017-09-28 17:10:46 +0100

    Updated processor tests

--- tests/processor.php
+++ tests/processor.php
@@ -14,15 +14,28 @@
 				$procInfo[str_replace(' ', '_', trim($key))] = trim($val);
 			}
 		}
-		
+
 		# Processor type
 		$Proc_type = $procInfo['model_name'];
 		
 		$Proc_cores = $procInfo['cpu_cores'];
 
+		//return array(
+		//	'type' => $Proc_type,
+		//	'cores' => $Proc_cores
+		//);
+
+		# Logical cores
+		$log_Cores = shell_exec("lscpu -p | egrep -v '^#' | wc -l");
+		
+		# Physical cores
+		$phy_Cores = shell_exec("lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l");
+		
 		return array(
 			'type' => $Proc_type,
-			'cores' => $Proc_cores
+			'cores' => $Proc_cores,
+			'log_cores' => $log_Cores,
+			'phy_cores' => $phy_Cores
 		);
 	}
 ?>
