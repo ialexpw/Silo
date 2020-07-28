6f156db00c85c5d9808e1359ee59f145d92e7ff5
Alex White <alex@picotory.com>
2020-07-28 14:22:27 +0100

    Test api

--- api.php
+++ api.php
@@ -4,11 +4,40 @@
 	# Load the config file
 	$mCfg = $Montr->LoadConfig();
 
-	$apiConf = array(
+	$apiConf = array();
 
-	);
+	# Server name
+	if(!empty($mCfg['server']['name'])) {
+		$apiConf['server']['name'] = $mCfg['server']['name'];
+	}
+
+	# Build the drive array
+	for($i=0;$i<5;$i++) {
+		if(!empty($mCfg['disks'][$i]['name']) && !empty($mCfg['disks'][$i]['location']) && isset($mCfg['disks'][$i]['limit'])) {
+			$apiConf['disks'][$i]['name'] = $mCfg['disks'][$i]['name'];
+			$apiConf['disks'][$i]['location'] = $mCfg['disks'][$i]['location'];
+			$apiConf['disks'][$i]['limit'] = $mCfg['disks'][$i]['limit'];
+		}
+	}
+
+	# Memory usage alert percentage
+	if(isset($mCfg['limits']['memory_usage'])) {
+		$apiConf['limits']['memory_usage'] = $_POST['mem_level'];
+	}
+	
+	# Load alert level
+	if(isset($mCfg['limits']['load_alert'])) {
+		$apiConf['limits']['load_alert'] = $_POST['load_level'];
+	}
+	
+	# Encode the array into json
+	$apiConf = json_encode($apiConf);
 
 	echo '<pre>';
 	print_r($mCfg);
+	echo '</pre>';
+
+	echo '<pre>';
+	print_r($apiConf);
 	echo '</pre>';
 ?>
